297f42a8f96595c9cfab66963f1df3ea603855ee
kimsimon902 <simon_kim@dlsu.edu.ph>
2021-05-19 21:00:15 +0800

    regularprofile activity

---
+++ app/src/main/java/com/mobdeve/kims/helpinghand/regularProfile.java
@@ -0,0 +1,14 @@
+package com.mobdeve.kims.helpinghand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class regularProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regular_profile);
+    }
+}
