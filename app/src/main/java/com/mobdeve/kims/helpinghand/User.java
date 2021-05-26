5bbdd5a05a7454595a6822e0632b13d5ab3fd068
Kyle <kyle_lagado@dlsu.edu.ph>
2021-05-26 19:41:45 +0800

    Added upload image/choose from gallery for user registration Working upload image to firebase Layout for add post activity

--- app/src/main/java/com/mobdeve/kims/helpinghand/User.java
+++ app/src/main/java/com/mobdeve/kims/helpinghand/User.java
@@ -1,19 +1,23 @@
 package com.mobdeve.kims.helpinghand;
 
+import android.net.Uri;
+
 public class User {
 
-    public String username, email, description;
+    public String username, email, description, imageName;
     public Boolean isOwner;
+
 
     public User(){
 
     }
 
-    public User(String username, String email, String description, Boolean isOwner){
+    public User(String username, String email, String description, Boolean isOwner, String imageName){
         this.username = username;
         this.email = email;
         this.description = description;
         this.isOwner = isOwner;
+        this.imageName = imageName;
     }
 
 
